598846753dda1644d6ec22f114f56bf75738d0af
Joel Roske <joelroske@Owners-MacBook-Air.local>
2020-07-17 19:19:19 -0500

    error in index comments to comment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
-const feedbackReducer = (state = { feeling: 0, understanding: 0, support: 0, comments: '' }, action) => {
+const feedbackReducer = (state = { feeling: 0, understanding: 0, support: 0, comment: '' }, action) => {
     console.log('in feedbackReducer', action.payload)
     if (action.type === 'ADD_FEEDBACK') {
         return { ...state, ...action.payload }
